b69dda11d23cb27fc718b7845e4836ad60a58991
Sameera Leola <sameeraleola@gmail.com>
2020-04-20 21:39:45 -0700

    Moved doubly_linked_list into queue and stack dir

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -51,42 +51,104 @@ class DoublyLinkedList:
     """Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
+    # Method 1
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head 
+            self.head.prev = new_node
+            self.head = new_node
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
+    # Method 4
     def remove_from_head(self):
-        pass
+        # Must return the value that is in the current head before we delete it.
+        value = self.head.value
+        self.delete(self.head)
+        return(value)
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
+    # Method 2
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        # Must return the value that is in the current head before we delete it.
+        value = self.tail.value
+        self.delete(self.tail)
+        return(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head: # Check that it is not already the head
+            return
+        self.add_to_head(node.value)
+        self.delete(node) # delete the node
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail: # Check that it is not already the head
+            return
+        self.add_to_tail(node.value)
+        self.delete(node) # delete the node
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
+    # Method 3
     def delete(self, node):
-        pass
-        
+        ### Planning ###
+        # This is the only node so remove the pointers to it
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None 
+        # It's the head
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        # It's the tail
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        # It's in the middle
+        else:
+            node.delete()  # We only have to delete because we are being passed the node address (reference to the node)
+        self.length -= 1 
+
+
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        # How do we get max? Loop through the nodes
+        # Create a variable to hold max which is the head because we're starting at the beginning
+        max = self.head.value # TODO: Does this need to handle negative numbers
+        # Create a tracking variable to know where we are
+        current = self.head
+        # Loop using a while because we don't know how many times we have to loop.  For-Loops are good when we know how many loops we have to make.
+        while (current is not None):
+            # Compare the value in the node to the max found
+            if current.value > max:
+                max = current.value
+            current = current.next # Go to the next element in the DLL
+        return max # when we are done
+
+
