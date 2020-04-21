0e940271fa711ccd9cbb482a1bb68c53ccffa905
Sameera Leola <sameeraleola@gmail.com>
2020-04-20 19:45:39 -0700

    dll_stack created

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,24 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        '''
+        A DLL is a good choice to store our elements because will must add and remove elements from both ends of the stack
+        often.  The DLL will allow adding and removing elements without running into the size limits of an Array,
+        nor the additional efficiency hit of shifting the elements.
+        '''
+        # Create the linked list that will contain the queue
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size:
+            head_value = self.storage.head.value
+            self.storage.remove_from_head()
+            self.size -= 1
+            return head_value
 
     def len(self):
-        pass
+        return self.size
