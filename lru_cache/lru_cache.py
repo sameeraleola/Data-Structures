605297bad8423776828370d7edc46bfe9cb108dc
Sameera Leola <sameeraleola@gmail.com>
2020-04-23 08:53:41 -0700

    lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -7,7 +7,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.storage {}
+        self.ordering = DoublyLinkedList()
+        self.size = 0 # len(self.ordering)
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +20,11 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.storage:
+        #fetch the DLL node which is the value of this key
+        node = self.storage[key]
+        self.ordering.move_to_end(node))
+        return node.value[1]
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +37,15 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # check if the key is in the cache we need to overwrite the old value
+        if key in self.storage
+            Node.value = (key, value)
+            self.ordering.move_to_end(node)
+
+        # Must first evict least-recently used element
+        if self.size == self.limit:
+            oldest_key = self.ordering.head.value[0] # head is the oldest element
+            del self.storage[oldest_key] 
+            # remove the head node from the DLL
+            self.ordering.remove_from_head()
+            self.size -=1
