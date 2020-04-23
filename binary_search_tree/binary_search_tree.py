11be9a2c2a536f4baef178c24caaf354997887c4
Sameera Leola <sameeraleola@gmail.com>
2020-04-23 13:05:08 -0700

    Exploring Binary search

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -11,13 +11,37 @@ class BinarySearchTree:
         self.right = None
 
     # Insert the given value into the tree
+    # Populate the left side
     def insert(self, value):
-        pass
-
+        if value > self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+        else:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+        
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # Check for the value at the root (base case)
+        if target == self.value:
+            return True
+
+            # Look for the value on the right
+        if target > self.value:
+            if contains(self.right, self.right.value)
+                if target == self.right.value:
+                    return True
+                else:
+                    return False
+
+
+            
+        
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -26,7 +50,35 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        # apply the call back
+        cb(self.value)
+
+        # base case: the node has no children
+        # call the cb on the children of this node
+        # let's check that this node has children
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+    
+    def iterative_for_each(self, cb):
+        # need a while loop and a stack to push and pop elements.
+        stack = []
+        # must add the root of the tree to the stack so that the loop will run
+        stack.append(self)
+
+        # Use a while loop to limit the stack
+        while len(stack) > 0:
+            current_node = stack.pop()
+            #check if the right child exists
+            if current_node.right:
+                stack.append(current_node.right)
+            #check if the left child exists
+            if current_node.left:
+                stack.append(current_node.left)
+            # The cb has to be done after checking for a child
+            cb(current_node.value)
+
 
     # DAY 2 Project -----------------------
 
